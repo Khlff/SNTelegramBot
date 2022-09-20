c321e719b459daf10b2ad875fc4531ea39cd1a4c
Khlff <1Ghlmncxw3456>
2022-09-20 21:02:35 +0500

    Version 0.0

--- src/main/java/SN/pck/Bot.java
+++ src/main/java/SN/pck/Bot.java
@@ -2,11 +2,12 @@ package SN.pck;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
+
+import java.util.Objects;
 
 public class Bot {
     private final TelegramBot bot = new TelegramBot("5724149159:AAEQMBUa6mfTPLRPqdJTKiGQybCCJ4qp3QU");
@@ -15,8 +16,6 @@ public class Bot {
         // Register for updates
         bot.setUpdatesListener(updates -> {
             updates.forEach(this::process);
-            // ... process updates
-            // return id of last processed update or confirm them all
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
     }
@@ -24,13 +23,22 @@ public class Bot {
     private void process(Update update) {
         // Send messages
         Message message = update.message();
-        CallbackQuery callbackQuery = update.callbackQuery();
 
         BaseRequest request = null;
 
         if (message != null) {
-            long chatId = message.chat().id();
-            request = new SendMessage(chatId, "Hello!");
+
+            System.out.println(message);
+
+            if (Objects.equals(message.text(), "/start")) {
+                long chatId = message.chat().id();
+                request = new SendMessage(chatId, "This Bot tells you if a certain site or service is down. You can choose for up to 5 services for free. We monitor the services with the help of downdetector.com");
+            }
+
+            else {
+                long chatId = message.chat().id();
+                request = new SendMessage(chatId, "Hello!");
+            }
         }
 
         if (request != null) {
