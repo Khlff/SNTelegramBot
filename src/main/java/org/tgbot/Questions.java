2324ff668ce02cba93b1143328f9f414d5ff16d0
Svet M <67125915+securesvet@users.noreply.github.com>
2022-10-04 15:42:49 +0500

    Changes: problem with questions.put resolved twice

--- src/main/java/org/tgbot/Questions.java
+++ src/main/java/org/tgbot/Questions.java
@@ -3,10 +3,17 @@ package org.tgbot;
 import java.util.HashMap;
 
 public class Questions {
-    public static void main(String[] args) {
-        HashMap<String, Integer> questions = new HashMap<>();
+    Questions() {
         questions.put("Когда родился Александр Сергеевич пушкин?(год)", 1799);
         questions.put("Год развала СССР?", 1991);
         questions.put("Год крещения Руси?", 988);
     }
+    HashMap<String, Integer> questions = new HashMap<>();
+    void addQuestion(String stringQuestion, Integer intAnswer) {
+        questions.put(stringQuestion, intAnswer);
+    }
 }
+
+
+
+
